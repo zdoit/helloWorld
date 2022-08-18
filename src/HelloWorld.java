428bd2c73accbd4b3303521d0e72b61772d6442c
zdoit <48817061+zdoit@users.noreply.github.com>
2022-08-18 18:43:17 +0800

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("你好666");
-        System.out.println("你好666");
+        System.out.println("你好666ooo");
+        System.out.println("你好666git");
     }
 }
